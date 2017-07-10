795450bad8e62d53296b5903889097c5e1caebec
Matteo Latini <mtylty@gmail.com>
2017-07-10 16:13:22 +0200

    Add basic Solidus support

--- lib/bretelline/app_generator.rb
+++ lib/bretelline/app_generator.rb
@@ -1,7 +1,20 @@
-require 'rails/generators/rails/app/app_generator'
-
 module Bretelline
   class AppGenerator < Rails::Generators::AppGenerator
+    class_option :database, type: :string, aliases: "-d", default: "postgresql",
+                            desc: "Configure for selected database (options: #{DATABASES.join('/')})"
+
+    class_option :skip_test, type: :boolean, default: true,
+                             desc: "Skip Test Unit"
+
+    class_option :skip_system_test, type: :boolean, default: true,
+                                    desc: "Skip system test files"
+
+    class_option :skip_turbolinks, type: :boolean, default: true,
+                                   desc: "Skip turbolinks gem"
+
+    class_option :skip_action_cable, type: :boolean, aliases: "-C", default: true,
+                                     desc: "Skip Action Cable files"
+
     def source_paths
       [
         Rails::Generators::AppGenerator.source_root,
@@ -9,10 +22,34 @@ module Bretelline
       ]
     end
 
+    def finish_template
+      invoke :bretelline
+      super
+    end
+
+    def run_bundle
+      # Do nothing, it will be run by the bretelline command
+    end
+
+    def bretelline
+      bundle_command 'install'
+      invoke :create_database
+      build :install_engine
+      invoke :migrate_database
+    end
+
+    def create_database
+      bundle_command "exec rails db:create"
+    end
+
+    def migrate_database
+      bundle_command "exec rails db:migrate"
+    end
+
     protected
 
     def get_builder_class
-      Bretelline::AppBuilder
+      Bretelline::Builders::Solidus
     end
   end
 end
