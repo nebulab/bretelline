cd1e5db93d8beacd4cc014155ca2005addda7ac2
Alessio Rocco <alessio.rocco.lt@gmail.com>
2017-07-21 15:21:01 +0200

    better methods order

--- lib/bretelline/builders/solidus.rb
+++ lib/bretelline/builders/solidus.rb
@@ -17,6 +17,12 @@ module Bretelline
 
       private
 
+      def solidus
+        bundle_command "exec rails g spree:install --sample=false"
+        bundle_command "exec rails g solidus:auth:install"
+        bundle_command "exec rake railties:install:migrations"
+      end
+
       def rspec
         bundle_command "exec rails generate rspec:install"
         directory 'spec/support'
@@ -25,12 +31,6 @@ module Bretelline
         prepend_to_file 'spec/spec_helper.rb', "require 'support/simplecov'"
         empty_directory 'spec/pages/sections'
         empty_directory 'spec/factories'
-      end
-
-      def solidus
-        bundle_command "exec rails g spree:install --sample=false"
-        bundle_command "exec rails g solidus:auth:install"
-        bundle_command "exec rake railties:install:migrations"
       end
 
       def ci
