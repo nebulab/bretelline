7281967ab2b3f87c004e6983ed2f9b35ae35ccd3
Alessio Rocco <alessio.rocco.lt@gmail.com>
2017-07-25 12:54:30 +0200

    Fix paperclip spec helper template

--- lib/bretelline/templates/spec/support/paperclip.rb
+++ lib/bretelline/templates/spec/support/paperclip.rb
@@ -1,7 +1,6 @@
 require 'paperclip/matchers'
 
 RSpec.configure do |config|
-  config.include Helpers
   config.include Paperclip::Shoulda::Matchers
 
   Paperclip::Attachment.default_options[:storage] = 'filesystem'
